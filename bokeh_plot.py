1348e9602b44311ddd9ad6be7038dfb561391c46
xofbd <dfox09@gmail.com>
2017-12-15 15:32:02 -0500

    display alabama map using flask

--- bokeh_plot.py
+++ bokeh_plot.py
@@ -1,23 +1,36 @@
+# author: xofbd
+# date: December 15, 2017
+# file name: bokeh_plot.m
+
+from matplotlib import cm
 import pandas as pd
+import pickle
 
 from bokeh.embed import components
+from bokeh.plotting import figure
 from bokeh.io import show
 from bokeh.models import (
     ColumnDataSource,
     HoverTool,
-    ColorMapper
+    LinearColorMapper,
+    ColorBar
 )
 
-from bokeh.palettes import RdYlBu as palette
-# from bokeh.palettes import Viridis6 as palette
-from bokeh.plotting import figure
 
-palette = palette[11]
+def rgb_to_hex(cmap):
+    hex = []
+    N = 255
+
+    for i in xrange(N):
+        r, g, b, a = getattr(cm, cmap)(i)
+        hex.append('#%02x%02x%02x' % (N * r, N * g, N * b))
+
+    return hex
 
 
 def create_plot():
-    palette.reverse()
 
+    # get county lats, lons, and county names
     with open('data/counties.p', 'r') as fp:
         counties = pickle.load(fp)
 
@@ -26,13 +39,11 @@ def create_plot():
 
     county_names = [county['name'] for county in counties.values()]
 
-    # get election data
+    # get processed election data
     df = pd.read_csv('data/county_level_percentages.csv', delimiter=',')
-    county_rates = [df.loc[(df['County Name'] == county) & (df['Party Code'] == 'DEM')]['Percentage of Vote'].values[0]
-                    for county in county_names]
-
-    # county_rates = [unemployment[county_id] for county_id in counties]
-    color_mapper = ColorMapper(palette=palette)
+    county_rates = [df.loc[(df['County Name'] == county) & (
+        df['Party Code'] == 'DEM')]['Percentage of Vote'].values[0]
+        for county in county_names]
 
     source = ColumnDataSource(data=dict(
         x=county_xs,
@@ -41,6 +52,14 @@ def create_plot():
         rate=county_rates,
     ))
 
+    # set colormap to use
+    palette = rgb_to_hex('seismic')
+    palette.reverse()
+
+    color_mapper = LinearColorMapper(palette=palette, low=0, high=100)
+    color_bar = ColorBar(color_mapper=color_mapper, border_line_color=None)
+
+    # initialize figure object
     TOOLS = "pan, wheel_zoom, reset, hover, save"
 
     p = figure(
@@ -49,10 +68,15 @@ def create_plot():
     )
     p.grid.grid_line_color = None
 
+    # add county shapes and color patch
     p.patches('x', 'y', source=source,
               fill_color={'field': 'rate', 'transform': color_mapper},
               fill_alpha=0.7, line_color="white", line_width=0.5)
 
+    # display color bar
+    p.add_layout(color_bar, 'right')
+
+    # add hover with fields to display
     hover = p.select_one(HoverTool)
     hover.point_policy = "follow_mouse"
     hover.tooltips = [
@@ -60,11 +84,7 @@ def create_plot():
         ("Doug Jones", "@rate%"),
         ("(Long, Lat)", "($x, $y)"),
     ]
-    # return unemployment
-    # show(p)
-    # return components(p)
 
+    return components(p)
 
 create_plot()
-
-# need list of lats, longs, names, percentage

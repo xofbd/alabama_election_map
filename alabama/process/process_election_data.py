5fc4d9a7924675806ea654acb87e10bf20a81754
xofbd <dfox09@gmail.com>
2020-11-21 23:09:42 -0500

    Refactor process_election_data.py

--- alabama/process/process_election_data.py
+++ alabama/process/process_election_data.py
@@ -1,24 +1,34 @@
-# author: xofbd
-# date: December 13, 2017
-# file name: process_election_data.py
-#
-# The purpose of this script is to process the AL election data to visualize
-# the results on a county level using Bokeh.
+"""
+The purpose of this script is to process the AL election data to visualize the
+results on a county level using Bokeh.
+"""
+import os
 
 import pandas as pd
 
-# load excel data
-df_A = pd.read_excel('sosEnrExport.xlsx', sheetname=0)
-df_B = pd.read_excel('sosEnrExport.xlsx', sheetname=1)
 
-# only include senate race
-senate_contest_code = 1000900
-df_A = df_A.loc[df_A['Contest Code'] == senate_contest_code]
+def main(path_in, path_out):
+    """
+    Dump special senate race election data to disk as a CSV.
 
-# merge on county code
-columns_B = ['County Code', 'Ballots Cast']
-df = pd.merge(df_A, df_B[columns_B], on='County Code')
-df['Percentage of Vote'] = 100 * df['Votes'] / df['Ballots Cast']
+    The passed path of the data contains Alabama election data, more than just
+    the 2017 special senate election race data. This function reads the Excel
+    file and creates a CSV file of the relevant data needed to visualize the
+    results.
+    """
+    df_A = pd.read_excel(path_in, sheet_name=0)
+    df_B = pd.read_excel(path_in, sheet_name=1)
 
-# save DataFrame to csv file
-df.to_csv('data/county_level_percentages.csv', delimiter=',')
+    senate_contest_code = 1000900
+    columns_B = ['County Code', 'Ballots Cast']
+
+    df = (df_A.query('`Contest Code` == @senate_contest_code')
+              .merge(df_B[columns_B], on='County Code'))
+    df['Percentage of Vote'] = 100 * df['Votes'] / df['Ballots Cast']
+    df.to_csv(path_out, index=False)
+
+
+if __name__ == '__main__':
+    path_in = os.path.join('data', 'sosEnrExport.xlsx')
+    path_out = os.path.join('data', 'county_level_percentages.csv')
+    main(path_in, path_out)
